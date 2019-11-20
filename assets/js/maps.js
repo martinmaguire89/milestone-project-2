4ecd4eed9db3f93a4b4896811c3e2a6cf2338885
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2019-11-20 19:52:55 +0000

    start of emailjs

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 8,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 54.591162795306,
+            lng: -6.39404296875
         }
     });
 
