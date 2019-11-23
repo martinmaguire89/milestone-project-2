9080ba648da46ed23faddf2d071d4dab3beb04d7
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2019-11-23 14:19:51 +0000

    markers added to maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,26 +1,50 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: {
-            lat: 54.591162795306,
-            lng: -6.39404296875
+    // Map options
+      var options = {
+        zoom:8,
+        center:{lat:54.787716,lng:-6.492314}
+      }
+
+      // New map
+      var map = new google.maps.Map(document.getElementById('map'), options);
+      //54.600783, -5.923918
+      // Array of markers
+      var markers = [
+        {
+          coords:{lat:54.600783,lng:-5.923918},
+          content:'<h1>https://www.malmaison.com/locations/belfast/</h1>'
+         },
+        {
+          coords:{lat:42.8584,lng:-70.9300},
+          content:'<h1>Amesbury MA</h1>'
+        },
+        {
+          coords:{lat:42.7762,lng:-71.0773}
         }
-    });
+      ];
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      // Loop through markers
+      for(var i = 0;i < markers.length;i++){
+        // Add marker
+        addMarker(markers[i]);
+      }
 
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+      // Add Marker Function
+      function addMarker(props){
+        var marker = new google.maps.Marker({
+          position:props.coords,
+          map:map,
         });
-    });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+        // Check content
+        if(props.content){
+          var infoWindow = new google.maps.InfoWindow({
+            content:props.content
+          });
+
+          marker.addListener('click', function(){
+            infoWindow.open(map, marker);
+          });
+        }
+      }
+    }
